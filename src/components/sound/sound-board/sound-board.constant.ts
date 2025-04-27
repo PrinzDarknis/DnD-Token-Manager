ea437879289f3d9d7dcef712a13730df4a0d9da6
Dennis Naujokat - Tower <prinzdarknis@googlemail.de>
2025-04-27 16:38:04 +0200

    add sounds

--- src/components/sound/sound-board/sound-board.constant.ts
+++ src/components/sound/sound-board/sound-board.constant.ts
@@ -1,11 +1,65 @@
 import { Sound } from "../../../model";
 
 import bauImg from "/icons/sound/bau.svg";
+import foxImg from "/icons/sound/fox.svg";
+import dogImg from "/icons/sound/dog.svg";
+import screamImg from "/icons/sound/scream.svg";
+import secretImg from "/icons/sound/secret.svg";
+import giftImg from "/icons/sound/gift.svg";
+import chokoboImg from "/icons/sound/chokobo.svg";
+import exclamationImg from "/icons/sound/!.svg";
+import gooseImg from "/icons/sound/goose.svg";
+import victoryImg from "/icons/sound/victory.svg";
 
 export const Sounds: Sound[] = [
   {
     name: "Bau Bau",
     img: bauImg,
     file: ["Fuwawa  Bau.mp3", "Mococo Bau.mp3"],
+  },
+  {
+    name: "Fox",
+    img: foxImg,
+    file: "fox.mp3",
+  },
+  {
+    name: "Dog",
+    img: dogImg,
+    file: "bark.mp3",
+  },
+  {
+    name: "Scream",
+    img: screamImg,
+    file: "scream.mp3",
+  },
+  {
+    name: "Secret",
+    img: secretImg,
+    file: "secret.mp3",
+  },
+  {
+    name: "Got Item",
+    img: giftImg,
+    file: "got item.mp3",
+  },
+  {
+    name: "Chokobo",
+    img: chokoboImg,
+    file: "chokobo.mp3",
+  },
+  {
+    name: "!",
+    img: exclamationImg,
+    file: "!.mp3",
+  },
+  {
+    name: "Honk",
+    img: gooseImg,
+    file: "honk.mp3",
+  },
+  {
+    name: "Victory",
+    img: victoryImg,
+    file: "victory.mp3",
   },
 ];

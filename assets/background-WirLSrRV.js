6adc8af82c46f15e10158480e0ff2d8f66134eed
Dennis Naujokat - Tower <prinzdarknis@googlemail.de>
2025-05-08 18:57:36 +0200

    Updates

--- assets/background-WirLSrRV.js
+++
@@ -1,1 +0,0 @@
-import{L as a,P as o,O as s}from"./index-Bzf7m602.js";a.info("Init",`Setup Backgroundtasks: ${o}`);s.setupBackground();
---
+++ assets/background-hMzXImmI.js
@@ -0,0 +1,1 @@
+import{L as a,P as o,O as s}from"./index-CQ6jJPv7.js";a.info("Init",`Setup Backgroundtasks: ${o}`);s.setupBackground();
